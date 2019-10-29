4cca7456169b5a5de46b05dbd0fc77e320b05f61
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 03:06:44 -0400

    version bump

--- package/indexing/package.js
+++ package/indexing/package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'aldeed:schema-index',
   summary: 'Control some MongoDB indexing with schema options',
-  version: '3.0.0',
+  version: '3.0.1',
   documentation: '../../README.md',
   git: 'https://github.com/aldeed/meteor-schema-index.git',
 });
 
 Package.onUse(function(api) {
   api.use([
-    'aldeed:collection2@3.0.0',
-    'ecmascript@0.6.1',
+    'aldeed:collection2@3.0.3',
+    'ecmascript@0.12.8',
   ]);
 
   api.mainModule('client.js', 'client');
